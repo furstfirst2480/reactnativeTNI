ebb144424738b909302849ae793a6e70ef315dbf
furstfirst2480 <me.supakrit_st@tni.ac.th>
2024-08-31 12:00:53 +0700

    Lecture8_StackNavigator

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
 import React, { useState } from "react";
-import {View, Text} from 'react-native'
+import { View, Text } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
-import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 import CreatePostScreen from "./screens/CreatePostScreen";
 
-const App=(): React.JSX.Element => {
+const App = (): React.JSX.Element => {
   const HomeStack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <HomeStack.Navigator>
-        <HomeStack.Screen name='Home' component={HomeScreen}/>
-        <HomeStack.Screen name='About' component={AboutScreen}/>
-        <HomeStack.Screen name='CreatePost' component={CreatePostScreen}/>
+      <HomeStack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ //Global
+          headerStyle: { backgroundColor: "#20b2aa" },
+          headerTintColor: "white",
+          headerTitleStyle: { fontWeight: "bold" },
+          // headerTitleAlign: "center",
+        }}
+      >
+        <HomeStack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "หน้าหลัก" }}
+        />
+        <HomeStack.Screen
+          name="About"
+          component={AboutScreen}
+           options={{title:'เกี่ยวกับเรา',
+          // headerStyle:{backgroundColor:'#20b2aa'},
+          // headerTintColor:'white',
+          // headerTitleStyle:{fontWeight:'bold'},
+          headerTitleAlign:'center'}}
+        />
+        <HomeStack.Screen name="CreatePost" component={CreatePostScreen} />
       </HomeStack.Navigator>
     </NavigationContainer>
   );
-}
+};
 export default App;
